0588903ecd75e9db446a71919a4d21f359df0c97
CJamesFoust <CJamesFoust@live.com>
2021-07-14 13:38:12 -0400

    first commit

---
+++ client/src/components/ProductList.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Link } from '@reach/router';
+import DeleteButton from './DeleteButton';
+
+export default props => {
+    const { removeFromDom } = props;
+
+    return (
+        <div>
+            {props.products.map((product, index) => {
+                return <p key={index}>
+                            <Link to={"/" + product._id}>
+                                { product.title }
+                            </Link>
+                            <DeleteButton 
+                                productId={product._id} 
+                                successCallback={ () => removeFromDom(product._id) }
+                            />
+                        </p>
+            })}
+        </div>
+    )
+}
